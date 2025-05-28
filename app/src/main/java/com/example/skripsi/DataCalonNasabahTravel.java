4c7c14fe2a13041522f48a4c6a8f1dc9619abdf5
lijovi <jovita.tanti@gmail.com>
2025-05-28 14:07:57 +0700

    Bukti Pembayaran, Data Calon Nasabah Travel and Health

---
+++ app/src/main/java/com/example/skripsi/DataCalonNasabahTravel.java
@@ -0,0 +1,27 @@
+package com.example.skripsi;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DataCalonNasabahTravel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_data_calon_nasabah_travel);
+        ActionBar supportActionBar = getSupportActionBar();
+        supportActionBar.hide();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
